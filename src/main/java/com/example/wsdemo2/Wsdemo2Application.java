02e447eb15d17c7031f74f09b1bc97def547862e
KimInseok <dlstjr90>
2019-09-10 21:09:07 +0900

    first commit

---
+++ src/main/java/com/example/wsdemo2/Wsdemo2Application.java
@@ -0,0 +1,15 @@
+package com.example.wsdemo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@SpringBootApplication
+public class Wsdemo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Wsdemo2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wsdemo2/Wsdemo2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.wsdemo2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Wsdemo2ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
