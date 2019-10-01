54f72b1c2ac1f0fa41bd0d41d82fbe0feb7f07f1
KimInseok <dlstjr90>
2019-10-01 19:08:05 +0900

    war build

--- src/main/java/com/example/wsdemo2/Wsdemo2Application.java
+++ src/main/java/com/example/wsdemo2/Wsdemo2Application.java
@@ -2,14 +2,20 @@ package com.example.wsdemo2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-public class Wsdemo2Application {
+public class Wsdemo2Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Wsdemo2Application.class, args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Wsdemo2Application.class);
     }
 
 }
