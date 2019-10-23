ef62b5f514456b25b54962e0e5af2e06654c45f1
KimInseok <dlstjr90>
2019-10-23 18:27:27 +0900

    h2 database / jpa 추가

--- src/main/java/com/example/wsdemo2/model/Room.java
+++ src/main/java/com/example/wsdemo2/model/Room.java
@@ -2,10 +2,14 @@ package com.example.wsdemo2.model;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.util.UUID;
 
 @Data
+@Entity
 public class Room {
+    @Id
     private String roomId;
     private String name;
 
