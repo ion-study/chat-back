fc888ad120e0d1c97dd96ac9609d2680d48f6060
KimInseok <dlstjr90>
2019-12-02 15:17:05 +0900

    Custom Query

--- src/main/java/com/example/wsdemo2/repository/user/UserCustomRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.wsdemo2.repository.user;
-
-import com.example.wsdemo2.entity.User;
-
-import java.util.List;
-
-public interface UserCustomRepo {
-    List<User> getCustom(String userId);
-}
--- src/main/java/com/example/wsdemo2/repository/user/UserRepo.java
+++ src/main/java/com/example/wsdemo2/repository/user/UserRepo.java
@@ -2,8 +2,14 @@ package com.example.wsdemo2.repository.user;
 
 import com.example.wsdemo2.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface UserRepo extends JpaRepository<User, Integer>, UserCustomRepo {
+public interface UserRepo extends JpaRepository<User, Integer> {
+    @Query(nativeQuery = true, value="SELECT * FROM USER WHERE USER_ID = :user_id")
+    List<User> getCustom(@Param("user_id") String userId);
 }
